c2c734bd6e8396eca995bfd3f0b08d8552c22d2c
Daniel <dmcclean780@gmail.com>
2024-09-06 19:39:04 +0100

    firebase register added - not working correctly

--- src/App.js
+++ src/App.js
@@ -1,46 +1,54 @@
-import './App.css';
-import Form from './components/common/Form'
+import "./App.css";
+import Form from "./components/common/Form";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { useState } from "react";
+import { app } from "./firebase-config";
 import {
-  BrowserRouter as Router,
-  Routes,
-  Route
-} from "react-router-dom";
-import { useState } from 'react';
-
-
+  getAuth,
+  signInWithEmailAndPassword,
+  createUserWithEmailAndPassword,
+} from "firebase/auth";
 
 function App() {
-  
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('')
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
   const handleAction = (id) => {
-  console.log(id)
-  
-}
+    const authentication = getAuth();
+
+    if (id === 2) {
+      createUserWithEmailAndPassword(authentication, email, password)
+        .then((response) => {
+          console.log(response)
+        })
+    }
+  };
+
   return (
     <Router>
       <div className="App">
         <>
-        <Routes>
+          <Routes>
             <Route
-              path='/login'
+              path="/login"
               element={
                 <Form
                   title="Login"
                   setEmail={setEmail}
                   setPassword={setPassword}
                   handleAction={() => handleAction(1)}
-                />}
+                />
+              }
             />
             <Route
-              path='/register'
+              path="/register"
               element={
                 <Form
                   title="Register"
                   setEmail={setEmail}
                   setPassword={setPassword}
                   handleAction={() => handleAction(2)}
-                />}
+                />
+              }
             />
           </Routes>
         </>
