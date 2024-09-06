241b423b2880e8f23e664f503f8add110b611006
Daniel <dmcclean780@gmail.com>
2024-09-06 19:19:03 +0100

    initial tutorial steps complete up to useinf firebase functions

--- src/App.js
+++ src/App.js
@@ -1,24 +1,51 @@
-import logo from './logo.svg';
 import './App.css';
+import Form from './components/common/Form'
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route
+} from "react-router-dom";
+import { useState } from 'react';
+
+
 
 function App() {
+  
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('')
+  const handleAction = (id) => {
+  console.log(id)
+  
+}
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="App">
+        <>
+        <Routes>
+            <Route
+              path='/login'
+              element={
+                <Form
+                  title="Login"
+                  setEmail={setEmail}
+                  setPassword={setPassword}
+                  handleAction={() => handleAction(1)}
+                />}
+            />
+            <Route
+              path='/register'
+              element={
+                <Form
+                  title="Register"
+                  setEmail={setEmail}
+                  setPassword={setPassword}
+                  handleAction={() => handleAction(2)}
+                />}
+            />
+          </Routes>
+        </>
+      </div>
+    </Router>
   );
 }
 
