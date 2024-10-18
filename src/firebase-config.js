c08131f0faf45241e78a036cd5cc191d32e6600e
Daniel McClean <dmcclean780@gmail.com>
2024-10-18 19:03:18 +0100

    fixed?

--- src/firebase-config.js
+++ src/firebase-config.js
@@ -1,12 +1,12 @@
 import { initializeApp } from "firebase/app";
 
 const firebaseConfig = {
-    apiKey: process.env.API_KEY,
-    authDomain: process.env.AUTH_DOMAIN,
-    projectId: process.env.PROJECT_ID,
-    storageBucket: process.env.STORAGE_BUCKET,
-    messagingSenderId: process.env.MESSAGING_SENDER_ID,
-    appId: process.env.APP_ID,
-    measurementId: process.env.MEASUREMENT_ID
+  apiKey: "AIzaSyARdLTdzdrtd44toGAd2eW3fJjoBUHovzY",
+  authDomain: "wargaming-web-app.firebaseapp.com",
+  projectId: "wargaming-web-app",
+  storageBucket: "wargaming-web-app.appspot.com",
+  messagingSenderId: "611600759065",
+  appId: "1:611600759065:web:8f4dd960177d6d4cc2ed88",
+  measurementId: "G-YSW7WPWH96"
   };
 export const app = initializeApp(firebaseConfig);
